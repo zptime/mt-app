49ff382eea21a722d92909829c10f106381b4cc6
pzhang <pzhang@whtb.tech>
2020-03-11 15:21:51 +0800

    配置文件格式化

--- server/dbs/config.js
+++ server/dbs/config.js
@@ -1,32 +1,32 @@
 export default {
   dbs: 'mongodb://127.0.0.1:27017/mtapp',
   redis: {
-    get host() {
+    get host () {
       return '127.0.0.1'
     },
-    get port() {
+    get port () {
       return 6379
     }
   },
   smtp: {
-    get host() {
+    get host () {
       return 'smtp.qq.com'
     },
-    get port() {
+    get port () {
       return 587
     },
-    get user() {
+    get user () {
       return '**@qq.com'
     },
-    get pass() { // 授权码
-      return 'aa*****'
+    get pass () { // 授权码
+      return '***'
     },
-    get code() { // 验证码
+    get code () { // 验证码
       return () => {
         return Math.random().toString(16).slice(2, 6).toUpperCase()
       }
     },
-    get expire() { // 过期时间
+    get expire () { // 过期时间
       return () => {
         return new Date().getTime() + 60 * 60 * 1000
       }
